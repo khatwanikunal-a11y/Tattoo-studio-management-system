98100bdd40e33cde2e737f2f72b60eade323ab8a
khatwanikunal-a11y <khatwanikunal@gmail.com>
2026-06-07 22:58:34 +0100

    continue server js configuration setup

--- server.js
+++ server.js
@@ -2,12 +2,6 @@ require('dotenv').config();
 const { createApp } = require('./app');
 const connectDB = require('./config/db');
 
-const PORT = process.env.PORT || 3000;
 
-connectDB().then(() => {
-  const app = createApp();
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-    console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
-  });
-});
+app.listen(3000, function () {
+    process.stdout.write('Server running on http://127.0.0.1:3000\n');
